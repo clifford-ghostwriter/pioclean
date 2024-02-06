6716213092b1b7372ac9e38318b4ee4ad2036d7f
clifford izuchukwu <cliffizuchucks@gmail.com>
2024-02-06 04:33:12 +0100

    nav-updated

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -6,63 +6,79 @@ import { FaBars } from "react-icons/fa";
 const Nav = () => {
   return (
     <Wrapper>
-      <div className="nav__center">
-        <div className="nav__logo">
-          <img src={logo} alt="logo" />
-        </div>
+      <div className="nav__wrapper section-center">
+        <div className="nav__center">
+          <div className="nav__logo">
+            <img src={logo} alt="logo" />
+            <p className="nav__text">Pioclean</p>
+          </div>
 
-        <div className="nav__text">
-          <p>Pioclean</p>
+          {/* <div className="nav__text">
+            <p>Pioclean</p>
+          </div> */}
+          <button className="btn nav__btn">
+            <FaBars />
+          </button>
         </div>
       </div>
-      <button className="btn nav__btn">
-        <FaBars />
-      </button>
     </Wrapper>
   );
 };
 
 const Wrapper = styled.nav`
   background-color: var(--clr--primary);
-  font-size: var(--fs--small);
-  display: flex;
-  justify-content: space-between;
-  height: 7rem;
-  align-items: center;
-  min-width: max-content;
-  border: 1px solid pink;
-  /* justify-content: center; */
+  /* border: 1px solid red; */
+
+  .nav__wrapper {
+    padding: var(--fs-300);
+
+    align-items: center;
+    min-width: min-content;
+    border: 1px solid pink;
+  }
 
   .nav__center {
-    height: 100%;
+    padding: 1rem;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    /* min-width: min-content; */
+    border: 1px solid pink;
+    width: 100%;
     display: flex;
     /* align-items: center; */
-    gap: 1rem;
-    border: 1px solid red;
+    /* gap: 1rem; */
     flex-shrink: 0;
     align-content: stretch;
     border: 1px solid red;
   }
 
+  .nav__logo {
+    display: flex;
+  }
   .nav__logo > * {
-    border: 1px solid red;
+    /* border: 1px solid red; */
     flex-shrink: 0;
     /* align-content: stretch; */
     height: 100%;
   }
 
   .nav__text {
-    border: 1px solid red;
-    align-self: stretch;
+    /* border: 1px solid red; */
+    /* align-self: center; */
     text-transform: uppercase;
-    margin-top: 0.7rem;
+    /* margin-top: 0.7rem; */
+    padding-top: var(--fs-700);
+    font-size: var(--fs-650);
   }
 
   .nav__btn {
     background-color: transparent;
-    height: max-content;
-    align-self: center;
-    border: none;
+    /* height: max-content; */
+    /* border: 1px solid red; */
+    size: 4rem;
+    height: 100%;
+    font-size: var(--fs-700);
   }
 `;
 export default Nav;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,29 @@
 import React from "react";
+import styled from "styled-components";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <Wrapper>
+      {/* <div>
+        <p>hello world</p>
+      </div> */}
+    </Wrapper>
+  );
 };
+
+const Wrapper = styled.section`
+  background-color: pink;
+  border: 1px solid black;
+  /* min-width: max-content; */
+  /* width: 700px; */
+
+  div {
+    background-color: black;
+    color: white;
+    width: 90%;
+    /* word-break: keep-all; */
+    margin-inline: auto;
+  }
+`;
 
 export default Home;
