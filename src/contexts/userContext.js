20c87fbeb88a2248b763862913b13065f03265f2
clifford-ghostwriter <cliffizuchucks@gmail.com>
2024-02-27 11:31:23 +0100

    build update contd

--- src/contexts/userContext.js
+++ src/contexts/userContext.js
@@ -8,6 +8,10 @@ const initialUserState = {};
 export const UserProvider = ({ children }) => {
   const [state, dispatch] = useReducer(user_reducer, initialUserState);
 
+  const func = function () {
+    dispatch();
+  };
+
   return (
     <userContext.Provider value={{ ...state }}>{children}</userContext.Provider>
   );
