491897c94b9c583dfe77bf59f0aa75498441e064
clifford-ghostwriter <cliffizuchucks@gmail.com>
2024-04-03 15:18:21 +0100

    mend

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-import { PageHero } from "../components";
+// import { PageHero } from "../components";
 
 const Contact = () => {
   return (
